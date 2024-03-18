b99f1bf17abe369340681f51342825d54ebe6223
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-03-18 19:52:04 +0100

    metemos la información de libro

--- libro.py
+++ libro.py
@@ -1,1 +1,15 @@
-print("hola")
+class Libro:
+    def __init__(self, titulo, autor, ejemplares):
+        self.titulo = titulo
+        self.autor = autor
+        self.ejemplares = ejemplares
+
+    def informacion(self):
+        print(f"Título: {self.titulo}, autor: {self.autor}, ejemplares disponibles: {self.ejemplares}")
+
+    def prestar(self):
+        if self.ejemplares > 0:
+            self.ejemplares -= 1
+            print(f"Libro prestado: {self.titulo}, autor: {self.autor}")
+        else:
+            print(f"No tenemos ejemplares de {self.titulo}")
