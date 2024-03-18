c14ae83262a295c4abfe2423cd0fdad32b303a40
Izan Lozano <151909866+izaann3@users.noreply.github.com>
2024-03-18 19:53:00 +0100

    metemos la información de prestamos

--- prestamo.py
+++ prestamo.py
@@ -1,1 +1,9 @@
-print("hola")
+class Prestamo:
+    def __init__(self, libro, socio, fecha):
+        self.libro = libro
+        self.socio = socio
+        self.fecha = fecha
+
+    def registrar_prestamo(self):
+        print(f"Préstamo registrado: Libro {self.libro.titulo}, por el socio {self.socio.nombre}")
+        self.libro.prestar()
